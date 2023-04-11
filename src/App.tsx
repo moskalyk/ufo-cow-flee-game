975506ef048bb261f35fefa27d7e23c42eb67b18
moskalyk <morgan.moskalyk@protonmail.ch>
2023-04-11 13:04:07 -0400

    added cow flee controls

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import ufo from './ufo.png'
 import ray from './ray.png'
 import cow from './cow.png'
 
+function getRandomValue() {
+  return Math.floor(Math.random() * (520 - (-520) + 1) + (-520));
+}
+
 function ImageSlider() {
   const [position, setPosition] = useState(0);
   const [init, setInit] = useState(false)
@@ -20,10 +24,13 @@ function ImageSlider() {
   };
 
   React.useEffect(() => {
-    // if(!init){
+    if(!init){
+      setInterval(() => {
+        setPosition(getRandomValue())
+      }, 5000)
       // window.addEventListener("keydown", handleKeyDown, true);
-      // setInit(true)
-    // }
+      setInit(true)
+    }
   }, [])
 
   return (
@@ -60,11 +67,65 @@ function ImageSlider() {
   );
 }
 
+function CowControl() {
+
+  const [position, setPosition] = useState(0);
+  const [right, setRight] = useState<any>(null)
+  const [init, setInit] = useState(false)
+
+  const handleKeyDown = (event: any) => {
+    console.log(position)
+    console.log(position > -519)
+    if(position != 520 && position != 520*-1){
+      if (event.keyCode === 37) { // left arrow
+        setPosition(position - 40);
+        setRight(false)
+      } else if (event.keyCode === 39) { // right arrow
+        setPosition(position + 40);
+        setRight(true)
+      }
+    }else {
+      if(position == -520){
+        setPosition(position + 40);
+      }else {
+        setPosition(position - 40);
+      }
+    }
+  };
+
+  return(
+  <>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        outline: 'none'
+      }}
+      onKeyDown={handleKeyDown}
+      tabIndex={0}
+    >
+      <img src={cow} 
+        style={{
+          width: "14vw" , 
+          position: 'fixed', 
+          bottom: '20px',
+          left: `${position}px`,
+          transition: "left 2s",
+          marginTop: '84vh',
+          marginLeft: '40.5vw',
+          transform: `${right ? 'scaleX(-1)': 'scaleX(1)'}`
+        }}/>
+    </div>
+  </>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <ImageSlider/>
-      <img style={{width: "14vw" , position: 'fixed', bottom: '20px'}} src={cow} />
+      <CowControl/>
     </div>
   );
 }
