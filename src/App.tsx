1f0a44b7c0d0c082509f3084e2a7432963b39476
moskalyk <morgan.moskalyk@protonmail.ch>
2023-04-12 10:10:53 -0400

    added leaderboard

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import ufo from './ufo.png'
 import ray from './ray.png'
 import cow from './cow_with_rollerblades.png'
 
+import { SequenceIndexerClient } from '@0xsequence/indexer'
+
+const indexer = new SequenceIndexerClient('https://mumbai-indexer.sequence.app')
+
 function getRandomValue() {
   return Math.floor(Math.random() * (520 - (-520) + 1) + (-520));
 }
@@ -13,10 +17,74 @@ function isBetween(num: number, lowerBound: number, upperBound: number, threshol
   return Math.abs(num - lowerBound) <= threshold && Math.abs(num - upperBound) <= threshold;
 }
 
+function Leaderboard(){
+  const [init, setInit] = useState<boolean>(false)
+  const [leaderboard, setLeaderboard] = useState<any>(new Map())
+
+  const populateLeaderboard = () => {
+    setInterval(async () => {
+        // here we query the joy contract address
+        const contractAddress = '0x761D7728FB98Fe451800e0346C8C6b8590477CFD'
+
+        const filter = {
+            contractAddress: contractAddress
+        }
+
+        // query Sequence Indexer for all token transaction history on mumbai
+        const transactionHistory = await indexer.getTransactionHistory({
+            filter: filter
+        })
+
+        const leaderboardRaw = new Map()
+
+        transactionHistory.transactions.map((tx: any) => {
+          tx.transfers.map((transfer: any) => {
+            if(!leaderboard.has(transfer.to)){
+              leaderboardRaw.set(transfer.to, Number(BigInt(transfer.amounts[0]) / BigInt(1e18)))
+            }else {
+              leaderboardRaw.set(transfer.to, leaderboard.get(transfer.to)+Number(BigInt(transfer.amounts[0] / BigInt(1e18))))
+            }
+          })
+        })
+
+        setLeaderboard(leaderboardRaw)
+    }, 1000)
+  }
+
+  React.useEffect(() => {
+    if(!init){
+      populateLeaderboard()
+      setInit(true)
+    }
+  })
+  return (
+  <div className='leaderboard'>
+    <table>
+      <tr>
+        <th>Address</th>
+        <th>Score</th>
+      </tr>
+      {
+        Array.from(leaderboard.entries()).splice(0,5).sort( (x:any, y: any) => {
+          return y[1] - x[1];
+        }).map((peer: any, id: number) => {
+          return <tr key={id}>
+            <td>{peer[0].slice(0,6)}...</td>
+            <td>{(peer[1]).toString()}</td>
+          </tr>
+        })
+      }
+    </table>
+  </div>
+  )
+}
+
 function UfoSky(props: any) {
+
   const [position, setPosition] = useState(0);
   const [init, setInit] = useState(false)
   const [rayTeleport, setRayTeleport] = useState(false)
+
   React.useEffect(() => {
      let timer = setInterval(() => {
       const ufoPosition = getRandomValue()
@@ -27,13 +95,9 @@ function UfoSky(props: any) {
             if(isBetween(props.cowPosition, ufoPosition, ufoPosition, 120)){
               alert('GAME OVER')
               if(props.score > Number(localStorage.getItem('highscore'))){
-                console.log('setting high score')
                 localStorage.setItem('highscore', String(props.score) )
               }
               props.setScore(0)
-            }else {
-              console.log(props.score)
-              console.log( Number(localStorage.getItem('highscore')))
             }
             clearInterval(gameOverInterval)
           }, 200)
@@ -41,7 +105,6 @@ function UfoSky(props: any) {
             setRayTeleport(false)
           }, 2000)
         }, 1000, ufoPosition)
-
         props.setScore((prev: any) => prev + 10)
       }, 3000)
     return () => {
@@ -166,6 +229,7 @@ function App() {
 
   return (
     <div className="App">
+      <Leaderboard />
       <UfoSky score={score} setScore={setScore} cowPosition={cowPosition}/>
       <CowControl cowPosition={cowPosition} setCowPosition={setCowPosition} />
       <div className='instructions'>{'click on the cow and navigate with < > keys'}</div>
